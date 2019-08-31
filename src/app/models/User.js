33d7ce82333dd53fc845f5be859ed31644e273b9
Vinicius Okamoto <leo_kamoto@hotmail.com>
2019-08-31 14:30:35 -0300

    Commit Validação de entrada

---
+++ src/app/models/User.js
@@ -0,0 +1,32 @@
+import Sequelize, { Model } from 'sequelize';
+import bcyrpt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcyrpt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcyrpt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '1591ef4a733cdbdd71c19309fd6ac47b',
+  expiresIn: '7d',
+};
